87be97aebeb2aa3ff6f05ea37befc7c2abea6111
Herb Bowie <hbowie@mac.com>
2020-04-02 16:10:21 -0700

    Updated dependency to point to GitHub

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(
             name: "NotenikMkdown",
             targets: ["NotenikMkdown"]),
     ],
-    dependencies: [.package(path: "../NotenikUtils")],
+    dependencies: [.package(url: "https://github.com/hbowie/NotenikUtils", from: "0.1.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
